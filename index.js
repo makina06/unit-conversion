dcf45507181e7bc0bab98006960f24f8746ae988
Max <119096559+makina06@users.noreply.github.com>
2023-04-16 17:20:21 +1200

    Add multiple unit conversions and refactor conversion code
    - Add conversions between meters/feet, liters/gallons, and kilograms/pounds
    - Refactor the conversion code to use separate functions for each conversion
    - Use toFixed(3) to round all conversion results to 3 decimal places

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let number;
 const convertBtn = document.getElementById("convert-btn");
 const inputEl = document.getElementById("input-el");
 const meterFeetNumber = document.getElementById("conversion-meters-feet");
+const literGallonNumber = document.getElementById("conversion-liter-gallon");
+const kiloPoundNumber = document.getElementById("conversion-kilo-pound");
 
 function formulaMeterToFeet(value) {
   return value * 3.281;
@@ -17,11 +19,38 @@ function formulafeetToMeter(value) {
   return value / 3.281;
 }
 
+function formulaLiterToGallon(value) {
+  return value * 0.264;
+}
+function formulaGallontoLiter(value) {
+  return value / 0.264;
+}
+function formulaKilogramtopound(value) {
+  return value * 2.204;
+}
+function formulaPoundtoKilogram(value) {
+  return value / 2.204;
+}
+
 convertBtn.addEventListener("click", function () {
   number = inputEl.value;
   const calculationMeterFeet = formulaMeterToFeet(number);
   const calculationFeetMeter = formulafeetToMeter(number);
+  const calculationliterGallon = formulaLiterToGallon(number);
+  const calculationGallonliter = formulaGallontoLiter(number);
+  const calculationkiloPound = formulaKilogramtopound(number);
+  const calculationPoundKilo = formulaPoundtoKilogram(number);
   meterFeetNumber.innerHTML = `<p>
-      ${number} meters = ${calculationMeterFeet} feet | ${number} feet = ${calculationFeetMeter} meters
+      ${number} meters = ${calculationMeterFeet.toFixed(
+    3
+  )} feet | ${number} feet = ${calculationFeetMeter.toFixed(3)} meters
     </p>`;
+  literGallonNumber.innerHTML = `<p> ${number} liter = ${calculationliterGallon.toFixed(
+    3
+  )} gallon | ${number} gallon = ${calculationGallonliter.toFixed(
+    3
+  )} liters</p>`;
+  kiloPoundNumber.innerHTML = ` <p>${number} kilo = ${calculationkiloPound.toFixed(
+    3
+  )} pounds | ${number} pound = ${calculationPoundKilo.toFixed(3)} kilo</p>`;
 });
